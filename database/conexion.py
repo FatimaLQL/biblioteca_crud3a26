0a155f0e4cdebbd86e50d9472f94dec6b3d78929
FatimaLQL <dactycellquintero@gmail.com>
2026-06-15 10:46:22 -0600

    LibroDAO - obtener todos

--- database/conexion.py
+++ database/conexion.py
@@ -7,9 +7,9 @@ class Conexion:
     @staticmethod
     def obtener_conexion():
         return psycopg2.connect(
-            host=os.getenv("DB_HOST")
-            database=os.getenv("DB_NAME")
-            user=os.getenv("DB_USER")
-            password=os.getenv("DB_PASSWORD")
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
             port=os.getenv("DB_PORT")
         ) 
